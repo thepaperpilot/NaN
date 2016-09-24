564a58f55d127087162b6d0cad412e05d3e28631
NotALizard <gcb140330@utdallas.edu>
2016-09-24 14:33:09 -0500

    fixed Anthony's bullshit

--- nan/setup.py
+++ nan/setup.py
@@ -4,9 +4,7 @@ import py2exe
 setup(
 	name = 'NaN',
 	url = 'https://github.com/thepaperpilot/NaN',
-	windows = [{'script': "nan/core.py"}],
+	windows = [{'script': "core.py"}],
 	options = {'py2exe': {'packages': ['esper', 'pygame']}},
-	packages = ['nan'],
-	package_dir = {'': 'nan'},
     package_data={'': ['*.png', 'RobotoMono-Regular.ttf', 'README.txt']},
 )
