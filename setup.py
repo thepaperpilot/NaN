270413c57adf78943a370a63ced4d3a431ada702
thepaperpilot <thepaperpilot@gmail.com>
2016-09-23 23:15:41 -0500

    WIP

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    options = {'py2exe': {'packages': ['esper', 'pygame']}},
-    windows = [{'script': "core.py"}],
+	name = 'NaN',
+	url = 'https://github.com/thepaperpilot/NaN',
+	options = {'py2exe': {'packages': ['esper', 'pygame']}},
+	windows = [{'script': "core.py"}],
+	package_dir={'': 'nan'},
+	packages=[''],
+    package_data={'': ['*.png', 'RobotoMono-Regular.ttf']},
+    include_package_data=True,
 )
